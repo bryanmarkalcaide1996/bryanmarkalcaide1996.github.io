049faf8ab9ce05a9fc8ea66e42b1bd81ee3d4424
Bryan Mark Alcaide <bryanmarkalcaide0420@gmail.com>
2022-02-10 21:21:13 +0800

    added Procfile for heroku deployment

--- index.js
+++ index.js
@@ -76,6 +76,6 @@ app.get("/shop", (req, res) => {
   res.render("shop", { merchList: data.merch });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server initiated on port 3000");
 });
